87f9a6eda6cc110c6a980747bb235b931b88fc57
Pablo Olivares <pablolivares1502@gmail.com>
2024-06-24 21:39:43 +0200

    Update run.sh

--- code/run.sh
+++ code/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-commands=("./testtlbo" "./testtlbosw" "./testtlboe" "./testtlbob" "./testtlboebsw")
+commands=("./testtlbo" "./testtlbosw" "./testtlboe" "./testtlbob" "./testtlboebsw" "./testsolis")
 args=(10 30 50)
 
 for cmd in "${commands[@]}"; do
